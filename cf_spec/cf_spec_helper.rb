94bb5cfe553c904e83639893de3bfda57d8a1731
Jacques Chester and Rick Reilly <pair+jchester+rreilly@pivotallabs.com>
2014-09-02 10:13:33 -0400

    Fix rspec-retry version to one compatible with rspec 3

--- cf_spec/cf_spec_helper.rb
+++ cf_spec/cf_spec_helper.rb
@@ -5,7 +5,3 @@ require 'rspec/retry'
 
 `mkdir -p log`
 Machete.logger = Machete::Logger.new("log/integration.log")
-
-RSpec.configure do |config|
-  config.verbose_retry = true # show retry status in spec process
-end
